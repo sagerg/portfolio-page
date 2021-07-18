c679d20dea91091e715ddc0e4e67a50392f73ebc
Sage Garcia <sage.garcia21@gmail.com>
2021-07-18 02:35:08 -0400

    revamped projects section

--- react-app/src/About.js
+++ react-app/src/About.js
@@ -4,7 +4,7 @@ import photo from './common/photo.jpeg';
 import TextLoop from "react-text-loop";
 
 const HEADER = `
-Hi there, I'm 
+Hey, I'm 
 `;
 
 const CONTENT = `
@@ -12,6 +12,7 @@ I'm a senior majoring in Computer Science at the University of Maryland Baltimor
 I started my journey in software development as a UI designer and community manager at User Experience Society, a student-run organization based in Manila.
 I also previously worked at Apple as a Technical Expert. My work consisted of end-to-end troubleshooting, repairing, and testing customers' devices.
 On the summer of my junior year, I joined the Software Engineer Program at JPMorgan Chase & Co. as an intern where I worked on web applications using React and Flask. Agile development, unit & integrated testing, and deployment via CI/CD pipelines with Jenkins were all core components of my work at Chase.
+I'm currently an organizer for HackUMBC where I closely work with external organizations to bring opportunities to our hackathon's attendees.
 `;
 
 class About extends React.Component {
@@ -27,13 +28,13 @@ class About extends React.Component {
               <Container>
                 <Typography variant="h5" gutterBottom>
                   {HEADER}
-                  <TextLoop interval={3500}>
+                  <TextLoop interval={3000}>
                     <span>Sage Garcia!</span>
                     <span>an aspiring developer</span>
-                    <span>a UX enthusiast</span>
-                    <span>a budding designer</span>
-                    <span>a tech geek</span>
-                    <span>a webcomic nerd</span>
+                    <span>a UI/UX enthusiast</span>
+                    <span>an Agile practitioner</span>
+                    <span>an Open Source supporter</span>
+                    <span>an AI/ML follower</span>
                   </TextLoop>
                   <span className="blinking-span-el text-primary" style={{color: '#f44336'}}> | </span>
                 </Typography>
--- react-app/src/Projects.js
+++ react-app/src/Projects.js
@@ -1,22 +1,160 @@
 import React from 'react';
-import { Container, Typography } from '@material-ui/core';
-import ProjectContent from './ProjectContent';
+import { Container, Typography, Paper } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import Siklab from './common/siklab.png';
+import WebDev from './common/webdev.png';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
+import CallMadeIcon from '@material-ui/icons/CallMade';
+import { Chip } from '@material-ui/core';
 
 const HEADER = `
 Notable Projects
 `;
 
-class Projects extends React.Component {
-    render() {
-      return (
+const CONTENT = {
+  "portfolio": 
+    {
+      "title": "Portfolio Website",
+      "desc": `
+      My portfolio website was developed using React, React Hooks, and Material UI. 
+      It's currently hosted using GitHub Pages.
+      Before deployment, unit testing was done using the React library Jest.
+      `,
+      "url": "https://github.com/sagerg/sagerg.github.io",
+      "btn": "Github Repo",
+      "alt": "photo of portfolio",
+      "photo": "",
+      "chips": 
         <div>
-          <Container>
-            <Typography variant="h5" gutterBottom>{HEADER}</Typography>
-            <ProjectContent />
-          </Container>
+          <Chip label="REACT" style={{backgroundColor: '#61DBFB', color: '#1F232A'}}/>
+          {'  '}
+          <Chip label="MATERIAL UI" style={{backgroundColor: '#1976D2', color: '#FFF'}}/>
         </div>
-      );
-    }
+    },
+  "siklab": 
+    {
+      "title": "Siklab Pilipinas",
+      "desc": `
+      In 2018, I co-lead the design team during the development of the project’s proof-of-concept, collaborating directly with the client and co-designers to develop the appropriate UI/UX direction.
+      `,
+      "url": "https://siklab.org.ph/",
+      "btn": "Siklab Pilipinas",
+      "alt": "photo of siklab project",
+      "photo": Siklab,
+      "chips": 
+        <div>
+          <Chip label="SKETCH" style={{backgroundColor: '#F9A52C', color: '#FFF'}}/>
+          {'  '}
+          <Chip label="HTML/CSS" style={{backgroundColor: '#F05629', color: '#FFF'}}/>
+        </div>
+    },
+  "health":
+    {
+      "title": "Health Journal",
+      "desc": `
+      The Health Journal is aimed at spreading mental and physical health awareness.
+      This website provides helpful online posts for creating a balanced and productive environment for anyone to stay and work in.
+      `,
+      "url": "https://phpmysql.howardcc.edu/Users/sgarcia6599/",
+      "btn": "The Health Journal",
+      "alt": "photo of health journal project",
+      "photo": WebDev,
+      "chips": 
+        <div>
+          <Chip label="SKETCH" style={{backgroundColor: '#F9A52C', color: '#FFF'}}/>
+          {'  '}
+          <Chip label="HTML/CSS" style={{backgroundColor: '#F05629', color: '#FFF'}}/>
+        </div>
+    },
+  "pass_manager":
+    {
+      "title": "Password Manager",
+      "desc": `
+      Command line tool, written in C++ for UNIX systems, parses account username and passwords. Securely encrypts data locally using Base64 encoding and an implementation of Vigenère Cipher.
+      `,
+      "url": "https://github.com/sagerg/cmd-line-tools#password-manager",
+      "btn": "Github Repo",
+      "alt": "photo of pass manager",
+      "photo": "",
+      "chips": 
+      <div>
+        <Chip label="C++" />
+        {'  '}
+        <Chip label="UNIX" style={{backgroundColor: '#333333', color: '#FFF'}}/>
+      </div>
+    },
+};
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+class InnerContent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photo: this.props.photo,
+      altText: this.props.altText,
+      mainContent: this.props.mainContent,
+      btnName: this.props.btnName,
+      url: this.props.url
+    };
   }
 
-  export default Projects;
+  render() {
+    return (
+      <div style={{marginTop: '10px'}}>
+            <Container>
+              {/* <img src={this.state.photo} alt={this.state.altText}></img> */}
+              <Typography variant="body1" gutterBottom>{this.state.mainContent}</Typography>
+              <Button color="secondary" variant="text" href={this.state.url}>
+                {this.state.btnName}&#160;
+                <CallMadeIcon />
+              </Button>
+            </Container>
+      </div>
+    );
+  }
+}
+
+export default function Projects() {
+  const classes = useStyles();
+  return (
+    <div>
+      <Container>
+        <Typography variant="h4" gutterBottom>{HEADER}</Typography>
+          <Grid container spacing={3}>
+          {
+            Object.keys(CONTENT).map((key, index) => {
+              return (
+                <Grid key={index} item md={6} sm={12}>
+                  <Paper className={classes.paper}>
+                    <Typography variant="h5">
+                      {CONTENT[key]["title"]}
+                      {CONTENT[key]["chips"]}
+                    </Typography>
+                    <InnerContent
+                      photo = {CONTENT[key]["photo"]}
+                      altText = {CONTENT[key]["alt"]}
+                      mainContent = {CONTENT[key]["desc"]}
+                      btnName = {CONTENT[key]["btn"]}
+                      url = {CONTENT[key]["url"]}
+                    /> 
+                  </Paper>
+                </Grid>
+              );
+            })
+          }
+          </Grid>
+      </Container>
+    </div>
+  );
+}
